8a064a8ea204a64c7df23eec4fa26c58837ebbe0
ILESANMI AYOMIDE <155600307+Ilesanmiayomide@users.noreply.github.com>
2024-01-13 01:28:01 +0100

    Add files via upload

---
+++ bible quiz/js/main.js
@@ -0,0 +1,11 @@
+function Open(){
+    a = document.getElementById("dashboard");
+    a.style.display ="inline";
+}
+
+function Close(){
+    var a;
+    a = document.getElementById("dashboard");
+    a.style.display="none";
+
+}
